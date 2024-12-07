33f042939840a0f5b3b6ae511e8a88d808ae337e
Anastasiya Mazur <98967429+idgafd@users.noreply.github.com>
2024-12-07 18:19:26 +0200

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_1") == True:
-	message = "Hello, Aanstasiia!"
+	message = "Hello, Anastasiia!"
 else:
 	message = "Hello, World!"
 
